fbbce1df0ab9dc25e695cf9f2828a627c7a5e75f
Aung Zin Phyo <aungzinphyo94@gmail.com>
2020-01-17 17:01:43 +0630

    Navigate Only

---
+++ app/src/main/java/com/azp/androidnavigation/TitleFragment.kt
@@ -0,0 +1,34 @@
+package com.azp.androidnavigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var root: View = inflater.inflate(R.layout.fragment_title, container, false)
+
+        var btnPlay = root.findViewById<Button>(R.id.btn_play)
+        btnPlay.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        // Inflate the layout for this fragment
+        return root
+    }
+
+
+}
